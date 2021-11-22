965f7b266591c397b7aced1eaf50b83e4956b980
Anna Bondarenko <anna93bondarenko@gmail.com>
2021-11-22 12:34:16 +0200

    add main block

--- src/views/Home/Categoties/Categories.jsx
+++ src/views/Home/Categoties/Categories.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import categories from './categories.json';
 import { ReactComponent as ArrowAll } from './arrow-all.svg';
 import css from './categories.module.css';
@@ -5,7 +6,7 @@ const Categories = () => {
   return (
     <>
       <div className={css.categNav}>
-        <p className={css.title}>Категории книг</p>
+        <h3 className={css.title}>Категории книг</h3>
         <ul className={css.categList}>
           {categories.map(({ id, name }) => (
             <li key={id} className={css.categ}>
@@ -13,10 +14,11 @@ const Categories = () => {
             </li>
           ))}
         </ul>
-        <div className={css.all}>
+
+        <NavLink to="/books" className={css.all}>
           <span className={css.textAll}>Все категории</span>
           <ArrowAll className={css.arrowAll} />
-        </div>
+        </NavLink>
       </div>
     </>
   );
--- src/views/Home/HomeView.jsx
+++ src/views/Home/HomeView.jsx
@@ -1,10 +1,80 @@
-import Categories from '../../components/Categoties/Categories';
+import { NavLink } from 'react-router-dom';
+import { FiChevronRight } from 'react-icons/fi';
+import Categories from './Categoties/Categories.jsx';
+import { ReactComponent as ArrowNext } from './arrow-next.svg';
+import css from './home.module.css';
 
 const HomeView = () => {
   return (
-    <>
+    <div className={css.container}>
       <Categories />
-    </>
+      <div className={css.mainBlock}>
+        <div className={css.new}>
+          <h2 className={css.newTitle}>Новинки книг</h2>
+          <NavLink to="/new" className={css.newLink}>
+            Смотреть все <FiChevronRight size="24px" />
+          </NavLink>
+        </div>
+        <ul className={css.booksList}>
+          <li className={css.bookItem}>
+            <img
+              src="http://dummyimage.com/180x271/c0c0c0&text=No+image"
+              alt=""
+              width="180"
+              height="271"
+            />
+            <p className={css.bookRating}>rating</p>
+            <h3 className={css.bookName}>name</h3>
+            <p className={css.bookAuthor}>author</p>
+          </li>
+          <li className={css.bookItem}>
+            <img
+              src="http://dummyimage.com/180x271/c0c0c0&text=No+image"
+              alt=""
+              width="180"
+              height="271"
+            />
+            <p className={css.bookRating}>rating</p>
+            <h3 className={css.bookName}>name</h3>
+            <p className={css.bookAuthor}>author</p>
+          </li>
+          <li className={css.bookItem}>
+            <img
+              src="http://dummyimage.com/180x271/c0c0c0&text=No+image"
+              alt=""
+              width="180"
+              height="271"
+            />
+            <p className={css.bookRating}>rating</p>
+            <h3 className={css.bookName}>name</h3>
+            <p className={css.bookAuthor}>author</p>
+          </li>
+          <li className={css.bookItem}>
+            <img
+              src="http://dummyimage.com/180x271/c0c0c0&text=No+image"
+              alt=""
+              width="180"
+              height="271"
+            />
+            <p className={css.bookRating}>rating</p>
+            <h3 className={css.bookName}>name</h3>
+            <p className={css.bookAuthor}>author</p>
+          </li>
+          <li className={css.bookItem}>
+            <img
+              src="http://dummyimage.com/180x271/c0c0c0&text=No+image"
+              alt=""
+              width="180"
+              height="271"
+            />
+            <p className={css.bookRating}>rating</p>
+            <h3 className={css.bookName}>name</h3>
+            <p className={css.bookAuthor}>author</p>
+          </li>
+        </ul>
+        <ArrowNext className={css.arrowNext} />
+      </div>
+    </div>
   );
 };
 export default HomeView;
